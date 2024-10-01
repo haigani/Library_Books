51051b03a9ff0c9c0ace7eb9eccdd7583a8baa9c
Hanieh Aigani <haniye_aygani@yahoo.com>
2024-10-01 18:23:56 -0400

    books urls added.

---
+++ books/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from .views import RegisterView, BookListCreateView, BookDetailView
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+urlpatterns = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('books/', BookListCreateView.as_view(), name='book-list-create'),
+    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+]
