8ac75bfeee9594dc395bc3c42a393d6d7bbcc515
Hanieh Aigani <haniye_aygani@yahoo.com>
2024-10-01 18:21:34 -0400

    books serializers added 2.

--- books/serializers.py
+++ books/serializers.py
@@ -1,5 +1,18 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Book
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
 
 class BookSerializer(serializers.ModelSerializer):
