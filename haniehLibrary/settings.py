d83b82ce553e9c62e719770ab3a561a4c9e99cfc
Hanieh Aigani <haniye_aygani@yahoo.com>
2024-10-01 17:37:17 -0400

    postgres settings in project.

--- haniehLibrary/settings.py
+++ haniehLibrary/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'haniehLibrary.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'library_book',
+        'USER': 'postgres',
+        'PASSWORD': 'hH@13670602',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
